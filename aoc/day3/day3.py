4cdd9e8f00dd037d401e4a619085c9a011db19c6
jonschwa <jon.schwartz@leaflink.com>
2020-12-03 01:03:55 -0500

    day 3!

---
+++ aoc/day3/day3.py
@@ -0,0 +1,65 @@
+from aoc.helpers import read_lines
+from functools import reduce
+
+
+class MapTraversal:
+
+    TREE_CHAR = "#"
+    SAFE_CHAR = "."
+
+    num_trees = 0
+
+    def __init__(self, tree_map, move_down, move_right):
+        self.tree_map = tree_map
+        self.move_down = move_down
+        self.move_right = move_right
+        self.map_height = len(self.tree_map)
+        self.map_width = len(self.tree_map[0])
+
+    def calc_move_right(self, col_num):
+        next_space = col_num + self.move_right
+        if next_space == self.map_width:
+            return 0
+        if next_space < self.map_width:
+            return next_space
+        else:
+            return next_space - self.map_width
+
+    def run(self):
+        """ until at the bottom of the map, move and determine if a tree was encountered """
+        row_num = 0
+        col_num = 0
+
+        while row_num < self.map_height:
+            if self.tree_map[row_num][col_num] == self.TREE_CHAR:
+                self.num_trees += 1
+            row_num += self.move_down
+            col_num = self.calc_move_right(col_num)
+
+        return self.num_trees
+
+
+multiply_slopes = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]
+
+test_map_panel = []
+for line in read_lines("day3/test.txt"):
+    test_map_panel.append([space for space in line.strip()])
+
+test_traversal = MapTraversal(test_map_panel, 1, 3)
+assert test_traversal.run() == 7
+
+test_results = [MapTraversal(test_map_panel, x, y).run() for [x, y] in multiply_slopes]
+assert reduce((lambda x, y: x * y), test_results) == 336
+
+
+part_1_map_panel = []
+for line in read_lines("day3/day3.txt"):
+    part_1_map_panel.append([space for space in line.strip()])
+
+part_1_traversal = MapTraversal(part_1_map_panel, 1, 3)
+print("part 1 num trees", part_1_traversal.run())
+
+part_2_results = [
+    MapTraversal(part_1_map_panel, x, y).run() for [x, y] in multiply_slopes
+]
+print("part 2 multiplied trees", reduce((lambda x, y: x * y), part_2_results))
